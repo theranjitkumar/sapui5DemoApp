ecd7a8665db2986e68d2f033f32ed6f16f52e8a1
theranjitkumar <web.ranjitkumar@gmail.com>
2020-05-03 19:05:21 +0530

    model implemented

--- webapp/controller/About.controller.js
+++ webapp/controller/About.controller.js
@@ -1,10 +1,45 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller"
-], function(
+], function (
 	Controller
 ) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.basicTemplate.controller.About", {
+		onInit: function () {
+			this.user = {
+				email: "",
+				password: ""
+			}
+			// var userModel = new sap.ui.model.json.JSONMoel();
+			// userModel.setData({ 'user': this.user });
+			// new sap.ui.getCore().setModel(userModel);
+
+		},
+
+		onBeforeRendering: function () {
+			console.log(this.getView().byId('output'));
+		},
+
+		onAfterRendering: function () {
+			this.getUsers();
+		},
+		getUsers: function () {
+			var aboutView = this.getView(); // .byId('idAbout');
+			$.get('https://jsonplaceholder.typicode.com/users').then(function (res) {
+				console.log(res);
+				var oModel = new sap.ui.model.json.JSONModel();
+				oModel.setData({ 'users': res });
+				// new sap.ui.getCore().setModel(oModel);
+				aboutView.setModel(oModel)
+				console.log(oModel.oData);
+
+
+			})
+		},
+		onExit: function () {
+
+		}
+
 	});
 });
